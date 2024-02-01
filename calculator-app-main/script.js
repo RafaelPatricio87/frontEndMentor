997387866babd0c2d352f131c91ebf2ec5c88267
Rafael <rafaelpatricio2002@gmail.com>
2024-02-01 03:03:32 -0300

    layout pronto

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -10,6 +10,7 @@ const calc = document.querySelector('.bg-calc');
 const del = document.querySelector('.del');
 const reset = document.querySelector('.reset');
 const igual = document.querySelector('.igual');
+const btn = document.querySelectorAll('.btn')
 
 
 
@@ -38,6 +39,8 @@ function mostrarTemaLigth(tema){
     numers.forEach(numer =>  numer.classList.remove("color-ligth-1","color-gray-1"))
     numers.forEach(numer =>  numer.classList.add("color-dark-1"))
     
+    btns.classList.remove('bg-ligth-3','bg-gray-2')
+    btns.classList.add('bg-dark-2')
     calc.classList.add("color-dark-1")
     header.classList.add("color-dark-1")
     thema.classList.add('color-dark-1')
@@ -47,6 +50,9 @@ function mostrarTemaLigth(tema){
     reset.classList.add('bg-dark-4')
     del.classList.add('bg-dark-4')
     igual.classList.add('bg-dark-green')
+    btn[2].style. backgroundColor = 'transparent'
+    btn[1].style. backgroundColor = 'transparent'
+    btn[0].style. backgroundColor = '#04DCCB'
     
   }
   if(tema === '2'){
@@ -58,7 +64,7 @@ function mostrarTemaLigth(tema){
     reset.classList.remove('bg-dark-4','bg-gray')
     del.classList.remove('bg-dark-4','bg-gray')
     igual.classList.remove('bg-dark-green','bg-gray-red')
-
+    // btns.classList.remove('bg-ligth-3')
     calc.classList.add('color-ligth-1')
     
     numers.forEach(numer =>  numer.classList.remove("color-gray-1","color-dark-1"))
@@ -72,7 +78,13 @@ function mostrarTemaLigth(tema){
     tela.classList.add("bg-ligth-2")
     thema.classList.add('color-ligth-1')
     body.classList.add("bg-ligth-1")
+    btns.classList.add('bg-ligth-3')
+
     containerTeclas.classList.add('bg-ligth-3')
+    btn[0].style. backgroundColor = 'transparent'
+    btn[2].style. backgroundColor = 'transparent'
+    btn[1].style. backgroundColor = '#C8885B'
+
   }
   if(tema === '3'){
     removerClasse()
@@ -83,6 +95,7 @@ function mostrarTemaLigth(tema){
     reset.classList.remove('bg-ligth-green','bg-dark-4','bg-ligth-4')
     del.classList.remove( 'bg-ligth-green','bg-dark-4','bg-ligth-4')
     igual.classList.remove('bg-ligth-orage','bg-gray-red','bg-ligth-orage','bg-ligth-4')
+    btns.classList.remove('bg-ligth-3')
     
     numers.forEach(numer =>  numer.classList.remove("color-dark-1","color-ligth-1"))
     numers.forEach(numer =>  numer.classList.add("color-gray-1"))
@@ -94,10 +107,15 @@ function mostrarTemaLigth(tema){
     tela.classList.add('bg-gray-2')
     body.classList.add("bg-gray-1")
     containerTeclas.classList.add('bg-gray-2')
-    
+    btns.classList.add('bg-gray-2')
+     
     reset.classList.add('bg-gray')
     del.classList.add('bg-gray')
     igual.classList.add('bg-gray-red')
+    btn[0].style. backgroundColor = 'transparent'
+    btn[1].style. backgroundColor = 'transparent'
+    btn[2].style. backgroundColor = '#CA4033'
+
     
   }
 }
